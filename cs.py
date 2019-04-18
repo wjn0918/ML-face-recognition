9097e72ce53d157efeb37b6866a5475dd808fe2b
wang <email@example.com>
2019-04-18 09:17:06 +0800

    人脸对比

--- cs.py
+++ cs.py
@@ -31,3 +31,4 @@ def appSearch():
 if __name__ == '__main__':
     # app.debug = True
     app.run(host='0.0.0.0', port=8987, debug=True)
+    open()
--- db.py
+++ db.py
@@ -1,34 +1,70 @@
+"""
+使用连接池连接数据库，执行sql
+
+"""
 import pymysql
 from DBUtils.PooledDB import PooledDB
 
+from pymongo import MongoClient
+
 from configparser import ConfigParser
 #读取配置
+from pymysql.cursors import DictCursor
+
 cf = ConfigParser()
 cf.read('conf/db.ini')
 
 
 def conn_mysql():
+    """
+    通过线程池，创建MySQL连接对象，线程数设为5
+    :return:
+    """
     items = dict(cf.items('mysql'))
     host = items['host']
     user_name = items['user_name']
     password = items['password']
     db = items['db']
-    pool = PooledDB(pymysql, 5, host=host, user=user_name, passwd=password, db=db, port=3306, setsession=['SET AUTOCOMMIT = 1'])  
+    pool = PooledDB(pymysql, 5, host=host, user=user_name, passwd=password, db=db, port=3306, setsession=['SET AUTOCOMMIT = 1'])
     # 5为连接池里的最少连接数，setsession=['SET AUTOCOMMIT = 1']是用来设置线程池是否打开自动更新的配置，0为False，1为True
     conn = pool.connection()
     return conn
 
 
-def executeSql(sql,args=None):
+def conn_mongodb():
+    """
+    获取mongodb连接
+    :return:
+    """
+    items = dict(cf.items('mongodb'))
+    host = items['host']
+    user_name = items['user_name']
+    password = items['password']
+    db = items['db']
+    client = MongoClient(host, 27017)
+    # 连接mydb数据库,账号密码认证
+    db = client.cs
+    # db.authenticate(user_name, password, db)
+    db.authenticate("root", "123456")
+    return db
+
+
+
+
+def executeSql(sql,args=None,returnDict=False):
     """
     执行sql
     :param sql:sql 语句
     :param args: sql 中的参数
+    :param returnDict: 是否创建返回字典类型游标
     :return: 查询的所有结果
     """
     conn = conn_mysql()
-    cursor = conn.cursor()
-    cursor.execute(sql,args)
+    if returnDict:
+        cursor = conn.cursor(DictCursor)
+    else:
+        cursor = conn.cursor()
+    cursor.execute(sql, args)
     r = cursor.fetchall()
     conn.commit()
     cursor.close()
@@ -36,13 +72,3 @@ def executeSql(sql,args=None):
     return r
     pass
 
-
-if __name__ == '__main__':
-    conn = conn_mysql()
-    cursor = conn.cursor()
-    sql = 'select * from face_recognition'
-    cursor.execute(sql)
-    print(cursor.fetchall())
-    conn.commit()
-    cursor.close()
-    conn.close()
